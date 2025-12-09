eba9ea2ed6c0c5d96273c6959413019761ea3d27
Angel <angelluislara@hotmail.es>
2025-12-09 22:16:47 +0100

    script de CD y cambio provisional para prueba

---
+++ deploy.ps1
@@ -0,0 +1,17 @@
+# Archivo: desplegar.ps1
+Write-Host "Iniciando despliegue manual a Azure Container Apps..." -ForegroundColor Cyan
+
+$APP_NAME = "ca-entangle-uclm-api"
+$GROUP = "rg-Entangle-UCLM"
+$IMAGE = "crEntangleUCLM.azurecr.io/tfg-backend:latest"
+
+Write-Host "Ordenando a Azure que descargue la nueva imagen..."
+az containerapp update --name $APP_NAME --resource-group $GROUP --image $IMAGE
+
+# 3. Confirmación
+if ($?) {
+    Write-Host "La actualización está en marcha." -ForegroundColor Green
+    Write-Host "Espera unos segundos y comprueba la web."
+} else {
+    Write-Host "Hubo un error. Asegúrate de haber hecho 'az login' antes." -ForegroundColor Red
+}
